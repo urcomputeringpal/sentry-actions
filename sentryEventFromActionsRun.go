76fe0a38e694515ec726dc99d76d38760f8d3972
Jesse Newland <jesse@jnewland.com>
2020-11-06 16:17:22 -0600

    construct a fake transaction based on tests

--- sentryEventFromActionsRun.go
+++ sentryEventFromActionsRun.go
@@ -9,13 +9,9 @@ import (
 	"github.com/google/go-github/v32/github"
 )
 
-func sentryEventFromActionsRun(ctx context.Context, workflowName string, owner string, repo string, runID int64, actions *github.ActionsService) (*sentry.Event, error) {
-	sentryEvent := sentry.NewEvent()
-	// TODO try reporting events as errors
-	sentryEvent.Level = "error"
-	// sentryEvent.Level = "info"
-	// sentryEvent.Type = "transaction"
+const transactionType = "transaction"
 
+func sentryEventFromActionsRun(ctx context.Context, workflowName string, owner string, repo string, runID int64, actions *github.ActionsService) (*sentry.Event, error) {
 	// wait for conclusion
 	conclusion := ""
 	var run *github.WorkflowRun
@@ -30,15 +26,61 @@ func sentryEventFromActionsRun(ctx context.Context, workflowName string, owner s
 			time.Sleep(10000 * time.Millisecond)
 		}
 	}
-
-	// decorate event with metadata
+	status := "ok"
 	if run.GetConclusion() == "failure" {
-		sentryEvent.Level = "error"
+		status = "internal_error"
+	}
+	if run.GetConclusion() == "cancelled" {
+		status = "cancelled"
+	}
+
+	description := fmt.Sprintf("%s/%s: %s (%s)", workflowName, run.GetEvent(), owner, repo)
+
+	var traceID string
+	if run.GetCheckSuiteURL() != "" {
+		traceID = run.GetCheckSuiteURL()
+	} else {
+		traceID = run.GetNodeID()
+	}
+
+	// TODO for each step
+	testSpan := &sentry.Span{
+		TraceID: traceID,
+		// step node id
+		SpanID: "1cc4b26ab9094ef0",
+		// ParentSpanID: run.GetNodeID(),
+		Description: `SELECT * FROM user WHERE "user"."id" = {id}`,
+		Op:          "db.sql",
+		Tags: map[string]string{
+			"function_name":  "get_users",
+			"status_message": "MYSQL OK",
+		},
+		StartTimestamp: run.GetCreatedAt().Time.UTC(),
+		EndTimestamp:   run.GetUpdatedAt().Time.UTC(),
+		Status:         "ok",
+		Data: map[string]interface{}{
+			"related_ids":  []uint{12312342, 76572, 4123485},
+			"aws_instance": "ca-central-1",
+		},
+	}
+
+	sentryEvent := &sentry.Event{
+		Type:           transactionType,
+		Spans:          []*sentry.Span{testSpan},
+		StartTimestamp: run.GetCreatedAt().Time.UTC(),
+		Timestamp:      run.GetUpdatedAt().Time.UTC(),
+		Transaction:    description,
+		Contexts: map[string]interface{}{
+			"trace": sentry.TraceContext{
+				TraceID:     traceID,
+				SpanID:      run.GetNodeID(),
+				Op:          run.GetEvent(),
+				Description: description,
+				Status:      status,
+			},
+		},
 	}
 	sentryEvent.Tags["WorkflowRun.conclusion"] = run.GetConclusion()
-	sentryEvent.StartTimestamp = run.GetCreatedAt().Time
-	sentryEvent.StartTimestamp = run.GetUpdatedAt().Time
-	sentryEvent.Message = fmt.Sprintf("%s/%s: %s (%s)", workflowName, run.GetEvent(), owner, repo)
 
 	return sentryEvent, nil
 }
