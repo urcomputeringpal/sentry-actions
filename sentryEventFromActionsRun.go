2c41bdce56e5b45b570ee137fa14dbd90f82c72f
Jesse Newland <jesse@jnewland.com>
2020-11-27 08:32:20 -0600

    correctly size ids

--- sentryEventFromActionsRun.go
+++ sentryEventFromActionsRun.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/getsentry/sentry-go"
@@ -11,7 +12,11 @@ import (
 
 const transactionType = "transaction"
 
-func sentryEventFromActionsRun(ctx context.Context, workflowName string, owner string, repo string, runID int64, actions *github.ActionsService) (*sentry.Event, error) {
+type actionsService interface {
+	GetWorkflowRunByID(ctx context.Context, owner string, repo string, runID int64) (*github.WorkflowRun, *github.Response, error)
+}
+
+func sentryEventFromActionsRun(ctx context.Context, workflowName string, owner string, repo string, runID int64, actions actionsService) (*sentry.Event, error) {
 	// wait for conclusion
 	conclusion := ""
 	var run *github.WorkflowRun
@@ -34,54 +39,50 @@ func sentryEventFromActionsRun(ctx context.Context, workflowName string, owner s
 		status = "cancelled"
 	}
 
-	description := fmt.Sprintf("%s/%s: %s (%s)", workflowName, run.GetEvent(), owner, repo)
+	description := fmt.Sprintf("%s/%s: %s (%s)", owner, repo, workflowName, run.GetEvent())
 
-	var traceID string
+	var traceSeed string
 	if run.GetCheckSuiteURL() != "" {
-		traceID = run.GetCheckSuiteURL()
+		traceSeed = run.GetCheckSuiteURL()
 	} else {
-		traceID = run.GetNodeID()
+		traceSeed = run.GetNodeID()
 	}
-
+	traceID := fmt.Sprintf("%x", generateTraceID(strings.NewReader(traceSeed)))
+	spanID := fmt.Sprintf("%x", generateSpanID(strings.NewReader(traceSeed)))
 	// TODO for each step
 	testSpan := &sentry.Span{
 		TraceID: traceID,
-		// step node id
-		SpanID: "1cc4b26ab9094ef0",
+		SpanID:  spanID,
+		// Check Suite if present
 		// ParentSpanID: run.GetNodeID(),
-		Description: `SELECT * FROM user WHERE "user"."id" = {id}`,
-		Op:          "db.sql",
-		Tags: map[string]string{
-			"function_name":  "get_users",
-			"status_message": "MYSQL OK",
-		},
+		Description:    description,
+		Op:             "actions.workflow",
 		StartTimestamp: run.GetCreatedAt().Time.UTC(),
 		EndTimestamp:   run.GetUpdatedAt().Time.UTC(),
 		Status:         "ok",
-		Data: map[string]interface{}{
-			"related_ids":  []uint{12312342, 76572, 4123485},
-			"aws_instance": "ca-central-1",
+		Tags: map[string]string{
+			"actions.event":      run.GetEvent(),
+			"actions.conclusion": run.GetConclusion(),
 		},
+		Data: map[string]interface{}{},
 	}
 
 	sentryEvent := &sentry.Event{
 		Type:           transactionType,
-		Spans:          []*sentry.Span{testSpan},
 		StartTimestamp: run.GetCreatedAt().Time.UTC(),
 		Timestamp:      run.GetUpdatedAt().Time.UTC(),
 		Transaction:    description,
+		Spans:          []*sentry.Span{testSpan},
 		Contexts: map[string]interface{}{
 			"trace": sentry.TraceContext{
 				TraceID:     traceID,
-				SpanID:      run.GetNodeID(),
+				SpanID:      spanID,
 				Op:          run.GetEvent(),
 				Description: description,
 				Status:      status,
 			},
 		},
-		Tags: map[string]string{
-			"WorkflowRun.conclusion": run.GetConclusion(),
-		},
+		Tags: map[string]string{},
 	}
 
 	return sentryEvent, nil
---
+++ sentryEventFromActionsRun_test.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/getsentry/sentry-go"
+	"github.com/google/go-github/v32/github"
+)
+
+type happyPathActionsClient struct {
+	workflowRun *github.WorkflowRun
+}
+
+type mockableActionsService interface {
+	GetWorkflowRunByID(ctx context.Context, owner string, repo string, runID int64) (*github.WorkflowRun, *github.Response, error)
+	MockWorkflowRun(*github.WorkflowRun)
+}
+
+type test struct {
+	name           string
+	actionsService mockableActionsService
+	workflowRun    *github.WorkflowRun
+	event          *sentry.Event
+	err            bool
+	wantTraceID    string
+	wantSpanID     string
+}
+
+var (
+	tests   []test
+	http200 = &github.Response{
+		Response: &http.Response{StatusCode: 200},
+	}
+)
+
+func (c *happyPathActionsClient) GetWorkflowRunByID(ctx context.Context, owner string, repo string, runID int64) (*github.WorkflowRun, *github.Response, error) {
+	return c.workflowRun, http200, nil
+}
+func (c *happyPathActionsClient) MockWorkflowRun(w *github.WorkflowRun) {
+	c.workflowRun = w
+}
+
+func init() {
+	tests = []test{
+		{
+			name:           "sample",
+			actionsService: &happyPathActionsClient{},
+			workflowRun: &github.WorkflowRun{
+				Conclusion: github.String("success"),
+				Event:      github.String("push"),
+				NodeID:     github.String("id"),
+				CreatedAt:  &github.Timestamp{time.Date(2020, time.January, 02, 15, 04, 05, 0, time.UTC)},
+				UpdatedAt:  &github.Timestamp{time.Date(2020, time.January, 02, 15, 04, 06, 0, time.UTC)},
+			},
+			event:       &sentry.Event{},
+			err:         false,
+			wantTraceID: "69640000000000000000000000000000",
+			wantSpanID:  "6964000000000000",
+		},
+	}
+}
+
+func TestTable(t *testing.T) {
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.actionsService.MockWorkflowRun(tt.workflowRun)
+			event, err := sentryEventFromActionsRun(context.Background(), "workflow", "owner", "repo", 123, tt.actionsService)
+
+			if tt.err && err == nil {
+				t.Errorf("%s: expected an error, didn't get one", tt.name)
+				return
+			}
+
+			if !tt.err && err != nil {
+				t.Errorf("%s: %+v", tt.name, err)
+				return
+			}
+			if event == nil {
+				t.Errorf("%s: event is nil", tt.name)
+				return
+			}
+			if tt.wantTraceID != event.Spans[0].TraceID {
+				log.Printf("%#v", event)
+				t.Errorf("%s.TraceID: want %s, got %s", tt.name, tt.wantTraceID, event.Spans[0].TraceID)
+				return
+			}
+			if tt.wantSpanID != event.Spans[0].SpanID {
+				log.Printf("%#v", event)
+				t.Errorf("%s.SpanID: want %s, got %s", tt.name, tt.wantSpanID, event.Spans[0].SpanID)
+				return
+			}
+		})
+	}
+}
